10768e1d1ced329b85cdc86ad561dc62720fc687
Brian Matamet <patrickmat101@gmail.com>
2020-02-17 12:16:13 -0800

    Created csv reader to parse by line

--- BankAccountApp/src/BankAccountApp.java
+++ BankAccountApp/src/BankAccountApp.java
@@ -1,14 +1,36 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 
 public class BankAccountApp {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Checking chacc1 = new Checking("Brian Matamet","123456789",2000,"brianmatamet@gmail.com");
-		Savings savacc1 = new Savings("Renzo Matamet","987654321",6000,"renzomat@gmail.com");
 		
-		chacc1.showInfo();
-		System.out.println();
-		savacc1.showInfo();
+		String filename = "users.csv";
+		File myUsers = new File(filename);
+		try {
+			Scanner in = new Scanner(myUsers);
+			while (in.hasNext()) {
+				String data = in.nextLine();
+				System.out.println(data);
+			}
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace(); 	
+		}
+		
+		// TODO Auto-generated method stub
+//		Checking chacc1 = new Checking("Brian Matamet","123456789",2000,"brianmatamet@gmail.com");
+//		chacc1.showInfo();
+//		System.out.println();
+//		Savings savacc1 = new Savings("Renzo Matamet","987654321",6000,"renzomat@gmail.com");
+//		savacc1.showInfo();
+		
+		//creating a proper data structure to store the list ***linked list is used for easy insertion/deletion***
+		
 	}
 
 }
